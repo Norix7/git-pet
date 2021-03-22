9f825c709fd4546683c0d5da8a00178a00fc61ff
Norix7 <norix7@gmail.com>
2021-03-22 23:50:46 +0900

    feat(functions): git-hook追加

---
+++ functions/src/git-hook.function.ts
@@ -0,0 +1,8 @@
+import * as functions from 'firebase-functions';
+
+export const gitHook = functions
+  .region('asia-northeast1')
+  .https.onRequest((request, response) => {
+    console.log(request.body);
+    response.send('success');
+  });
